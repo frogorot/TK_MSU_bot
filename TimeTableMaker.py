a24cd492420aef93740c8d4a456080405d59134f
Alexey Vorotnikov <asvorotnikov@kraftway.ru>
2022-10-05 22:12:45 +0300

    1) Добавлена загрузка информации о дистанциях из файла 2) В core_funcs изменены типы т.к. теперь живём не на eime, а на int. 3) interface переехал на табуляцию. 4) Сделан файл для загрузки дистанций

--- TimeTableMaker.py
+++ TimeTableMaker.py
@@ -3,53 +3,108 @@ admin_chat_id = {}
 
 #from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
-    Application,
-    CommandHandler,
-    #ContextTypes,
-    ConversationHandler,
-    MessageHandler,
-    filters,
+	Application,
+	CommandHandler,
+	#ContextTypes,
+	ConversationHandler,
+	MessageHandler,
+	filters,
 )
+from datetime import datetime 
+from datetime import timedelta 
+import time
 
 import interface
 import core_funcs as cf
 
+
 def main() -> None:
-    """ Configure bot."""
-    run_file = "run.ini"
-    run_pars = cf.Parser()
-    run_pars.load_toml(run_file)
 
-    sucere_pars = cf.Parser()
-    sucere_pars.load_toml(run_pars.at('secure_file'))
-    #info_pars = cf.Parser()
-    #info_pars.load_toml(run_pars.at('info_file'))
 
-    API_TOKEN =  sucere_pars.at('Token')
 
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(API_TOKEN).build()
+	""" Configure interface."""
+	""" Load conf files."""
+	run_file = "run.ini"
+	run_pars = cf.Parser()
+	run_pars.load_toml(run_file)
 
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", bot.user_reg_start)],
-        states={
-            bot.AGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.user_reg_age)],
-            bot.GENDER: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.user_reg_gender)],
-            bot.UNIVERSITY: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), bot.user_reg_university)],
-            bot.FACILITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.user_reg_facility)],
-            bot.DISTANCES: [MessageHandler(filters.Regex("^(Горная|Пешеходная|Водная|Вело|Охота на лис)$"), bot.user_reg_distances)],
-        },
-        fallbacks=[CommandHandler("cancel", bot.cancel)],
-    )
+	sucere_pars = cf.Parser()
+	sucere_pars.load_toml(run_pars.at('secure_file'))
+	info_pars = cf.Parser()
+	info_pars.load_toml(run_pars.at('info_file'))
+	secure_directory = run_pars.at('secure_dir')
+	info_directory = run_pars.at('info_dir')
 
-    application.add_handler(conv_handler)
-    print("Bot start")
+	""" Load data."""
+	""" Load distance data."""
+	pers_dist_group_name = 'personal_distances'
+	group_dist_group_name = 'group_distances'
+	open_time_str = "open_time" # часы:минуты
+	close_time_str = "close_time" # часы:минуты
+	interval_str = "interval" # минуты:секунды
+	passing_time_str = "passing_time" # минуты:секунды
 
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+	for p_dist in info_pars.at(pers_dist_group_name):
+		name = info_pars.at(pers_dist_group_name)[p_dist]
+		cf.dist_personal_dict[name] = cf.DistanceResults(name)
+		dist_params = info_pars.at(p_dist) 
+
+		open_time = int(dist_params[open_time_str][0:2]) * 3600 + int(dist_params[open_time_str][3:5]) * 60
+		close_time = int(dist_params[close_time_str][0:2]) * 3600 + int(dist_params[close_time_str][3:5]) * 60
+		interval = int(dist_params[interval_str][0:2]) * 60 + int(dist_params[interval_str][3:5])
+		passing_time = int(dist_params[passing_time_str][0:2]) * 60 + int(dist_params[passing_time_str][3:5])
+		#minutes=int(dist_params[open_time_str][3:4]), hours= int(dist_params[open_time_str][0:1]))
+		#open_time = datetime(year=2022, month=10, day=15, hour= int(dist_params[open_time_str][0:1]), minute= int(dist_params[open_time_str][3:4]), second = 0)
+		#close_time = datetime(year=2022, month=10, day=15, hour= int(dist_params[close_time_str][0:1]), minute= int(dist_params[close_time_str][3:4]), second = 0)
+		#interval = datetime(year=2022, month=10, day=15, hour= 0, minute= int(dist_params[open_time_str][0:1]), second = int(dist_params[passing_time_str][3:4]))
+		#passing_time = datetime(year=2022, month=10, day=15, hour= 0, minute= int(dist_params[open_time_str][0:1]), second = int(dist_params[passing_time_str][3:4]))
+
+		cf.time_table_dict[name] = cf.TimeTable(name, 
+												open_time= open_time,
+												close_time= close_time,
+												interval= interval,
+												passing_time= passing_time)
+	for g_dist in info_pars.at(group_dist_group_name):
+		name = info_pars.at(group_dist_group_name)[g_dist]
+		cf.dist_group_dict[name] = cf.DistanceResults(name)
+
+		open_time = int(dist_params[open_time_str][0:2]) * 3600 + int(dist_params[open_time_str][3:5]) * 60
+		close_time = int(dist_params[close_time_str][0:2]) * 3600 + int(dist_params[close_time_str][3:5]) * 60
+		interval = int(dist_params[interval_str][0:2]) * 60 + int(dist_params[interval_str][3:5])
+		passing_time = int(dist_params[passing_time_str][0:2]) * 60 + int(dist_params[passing_time_str][3:5])
+
+		cf.time_table_dict[name] = cf.TimeTable(name, 
+												open_time= open_time,
+												close_time= close_time,
+												interval= interval,
+												passing_time= passing_time)
+
+
+	API_TOKEN =  sucere_pars.at('Token')
+
+	"""Run the interface."""
+	# Create the Application and pass it your interface's token.
+	application = Application.builder().token(API_TOKEN).build()
+
+	# Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+	conv_handler = ConversationHandler(
+		entry_points=[CommandHandler("start", interface.user_reg_start)],
+		states={
+			interface.AGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, interface.user_reg_age)],
+			interface.GENDER: [MessageHandler(filters.TEXT & ~filters.COMMAND, interface.user_reg_gender)],
+			interface.UNIVERSITY: [MessageHandler(filters.Regex("^(Мальчик|Девочка)$"), interface.user_reg_university)],
+			interface.FACILITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, interface.user_reg_facility)],
+			interface.DISTANCES: [MessageHandler(filters.Regex("^(Горная|Пешеходная|Водная|Вело|Охота на лис)$"), interface.user_reg_distances)],
+		},
+		fallbacks=[CommandHandler("cancel", interface.cancel)],
+	)
+
+	application.add_handler(conv_handler)
+	print("Bot start")
+
+	# Run the interface until the user presses Ctrl-C
+	application.run_polling()
   
 
 if __name__ == "__main__":
-    main()
+	main()
--- interface.py
+++ interface.py
@@ -1,29 +1,33 @@
 import logging
+
+from datetime import datetime 
+from datetime import timedelta 
+
 import re
 
 from telegram import __version__ as TG_VER
 
 try:
-    from telegram import __version_info__
+	from telegram import __version_info__
 except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+	__version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
 
 if __version_info__ < (20, 0, 0, "alpha", 1):
-    print(__version_info__)
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
+	print(__version_info__)
+	raise RuntimeError(
+		f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+		f"{TG_VER} version of this example, "
+		f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+	)
 
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-    ConversationHandler,
-    MessageHandler,
-    filters,
+	Application,
+	CommandHandler,
+	ContextTypes,
+	ConversationHandler,
+	MessageHandler,
+	filters,
 )
 
 import core_funcs as cf
@@ -31,26 +35,19 @@ import core_funcs as cf
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO,
-    filename="py_log.log",filemode="w"
+	format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO,
+	filename="logs\py_log"+ datetime.today().strftime("-%m.%d.%Y,%H-%M-%S") + ".log",filemode="w"
 )
 logger = logging.getLogger(__name__)
 
 #user registration steps:
 AGE, GENDER, UNIVERSITY, FACILITY, DISTANCES = range(5)
 
-dist_dict = { "Горная" : cf.DistanceResults("Горная"), 
-              "Пешеходная" : cf.DistanceResults("Пешеходная"), 
-              "Водная" : cf.DistanceResults("Водная"),
-              "Вело" : cf.DistanceResults("Вело"),
-              "Охота на лис" : cf.DistanceResults("Охота на лис")}
-dist_print_pattern = [ [0,2,3],[1,4]]
-
 # Handle '/start' and '/help'
 # Дописать!!
 #@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    msg = bot.reply_to(message, """\
+	msg = bot.reply_to(message, """\
 Привет! Я бот горного Турклуба МГУ. Я совсем маленький, но быстро учусь!
 Мои команды:
 \start - Регистрация участника на слёт. Во время регистрации можно выбрать интересующие вас _личные_ дистанции.
@@ -58,130 +55,130 @@ def send_welcome(message):
 \my_dist - Список дистанций, на которые вы зарегистрированы.
 \ask_admin - Написанное после этой команды сообщение будет передано администратору. Может пригодится для связи по проблемам или предложениям и замечаниям.
 """)
-    #bot.register_next_step_handler(msg, process_name_step)
+	#bot.register_next_step_handler(msg, process_name_step)
 
 def processing_exceptions(message, Exception):
-    bot.reply_to(message, 'Что-то пошло не так( Попробуйте ещё раз!')
-    if not admin_chat_id == None:
-        bot.send_message(admin_chat_id, e.args)
+	bot.reply_to(message, 'Что-то пошло не так( Попробуйте ещё раз!')
+	if not admin_chat_id == None:
+		bot.send_message(admin_chat_id, e.args)
 
 # Handle '/ask_admin'
 #@bot.message_handler(commands=['ask_admin'])
 def process_ask_admin(message):
-    try:
-        msg = bot.reply_to(message, 'Пожалуйста, введите сообщение для Администратора.')
-        bot.register_next_step_handler(msg, process_send_to_admin)
-    except Exception as e:
-        processing_exceptions(message, e)
+	try:
+		msg = bot.reply_to(message, 'Пожалуйста, введите сообщение для Администратора.')
+		bot.register_next_step_handler(msg, process_send_to_admin)
+	except Exception as e:
+		processing_exceptions(message, e)
 
 def process_send_to_admin(message):
-    try:
-        bot.forwardMessage(chat_id = admin_chat_id, from_chat_id = message.chat.id, message_id = message.message_id)
-        msg = bot.reply_to(message, 'Ваше сообщение передано Администратору.')
-    except Exception as e:
-        processing_exceptions(message, e)
+	try:
+		bot.forwardMessage(chat_id = admin_chat_id, from_chat_id = message.chat.id, message_id = message.message_id)
+		msg = bot.reply_to(message, 'Ваше сообщение передано Администратору.')
+	except Exception as e:
+		processing_exceptions(message, e)
 
 
 async def user_reg_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and asks the user about their name."""
-    await update.message.reply_text(
-       "Ура, давайте знакомится!"
-       "Отправьте /cancel чтобы прекратить общение.\n\n"
-       "Как вас зовут? Пожалуйста, напишите полные имя и фамилию.",
-    )
-    return AGE
+	"""Starts the conversation and asks the user about their name."""
+	await update.message.reply_text(
+	   "Ура, давайте знакомится!"
+	   "Отправьте /cancel чтобы прекратить общение.\n\n"
+	   "Как вас зовут? Пожалуйста, напишите полные имя и фамилию.",
+	)
+	return AGE
 
 async def user_reg_age(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected name and asks for a age."""
-    user = update.message.from_user
-    logger.info("Name of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "Приятно познакомится! "
-        "Сколько вам лет?."
-    )
+	"""Stores the selected name and asks for a age."""
+	user = update.message.from_user
+	logger.info("Name of %s: %s", user.first_name, update.message.text)
+	await update.message.reply_text(
+		"Приятно познакомится! "
+		"Сколько вам лет?."
+	)
 
-    return GENDER
+	return GENDER
 
 async def user_reg_gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected age and asks for a gender."""
-    user = update.message.from_user
-    logger.info("Age of %s: %s", user.first_name, update.message.text)
+	"""Stores the selected age and asks for a gender."""
+	user = update.message.from_user
+	logger.info("Age of %s: %s", user.first_name, update.message.text)
 
-    reply_keyboard = [["Boy", "Girl", "Other"]]
-    
-    await update.message.reply_text(
-        "Пожалуйста, выберете ваш пол.",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
-        ),
-    )
-    
-    return UNIVERSITY
+	reply_keyboard = [["Мальчик", "Девочка"]]
+	
+	await update.message.reply_text(
+		"Пожалуйста, выберете ваш пол.",
+		reply_markup=ReplyKeyboardMarkup(
+			reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
+		),
+	)
+	
+	return UNIVERSITY
 
 async def user_reg_university(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected gender and asks for a university."""
-    user = update.message.from_user
-    logger.info("Univer of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "В каком вузе вы учитесь? "
-        "Если вы не студент, можете написать место работы или поставить \"-\".",
-        reply_markup=ReplyKeyboardRemove(),
-    )
+	"""Stores the selected gender and asks for a university."""
+	user = update.message.from_user
+	logger.info("Univer of %s: %s", user.first_name, update.message.text)
+	await update.message.reply_text(
+		"В каком вузе вы учитесь? "
+		"Если вы не студент, можете написать место работы или поставить \"-\".",
+		reply_markup=ReplyKeyboardRemove(),
+	)
 
-    return FACILITY
+	return FACILITY
 
 # Надо переделать!!!
 async def user_reg_facility(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected university and asks for a facility or for distances."""
-    user = update.message.from_user
-    logger.info("fasil of %s: %s", user.first_name, update.message.text)
-    is_msu = re.search("МГУ", update.message.text)
-    if is_msu != None:
-        await update.message.reply_text(
-            "На каком факультете вы учитесь? "
-        )
-        return DISTANCES
-    else:
-        reply_keyboard = [["Горная", "Вело", "Пешеходная"],
-                          ["Охота на лис", "Водная"],
-                          ["всё"]]
-        await update.message.reply_text(
-            "На каких дистанциях вы хотели бы участвовать?",
-            reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
-        ),
-        )
-        return DISTANCES
-    
+	"""Stores the selected university and asks for a facility or for distances."""
+	user = update.message.from_user
+	logger.info("fasil of %s: %s", user.first_name, update.message.text)
+	is_msu = re.search("МГУ", update.message.text)
+	if is_msu != None:
+		await update.message.reply_text(
+			"На каком факультете вы учитесь? "
+		)
+		return DISTANCES
+	else:
+		reply_keyboard = [["Горная", "Вело", "Пешеходная"],
+						  ["Охота на лис", "Водная"],
+						  ["всё"]]
+		await update.message.reply_text(
+			"На каких дистанциях вы хотели бы участвовать?",
+			reply_markup=ReplyKeyboardMarkup(
+			reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
+		),
+		)
+		return DISTANCES
+	
 async def user_reg_distances(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    user = update.message.from_user
-    if update.message.text == "всё":
-        await update.message.reply_text(
-            "Спасибо!",
-            reply_markup=ReplyKeyboardRemove(),
-            )
-        print(logger)
-        return ConversationHandler.END
-    else:
-        logger.info("Dist of %s: %s", user.first_name, update.message.text)
+	user = update.message.from_user
+	if update.message.text == "всё":
+		await update.message.reply_text(
+			"Спасибо!",
+			reply_markup=ReplyKeyboardRemove(),
+			)
+		print(logger)
+		return ConversationHandler.END
+	else:
+		logger.info("Dist of %s: %s", user.first_name, update.message.text)
 
-        reply_keyboard = [["Горная", "Вело", "Пешеходная"],
-                          ["Охота на лис", "Водная"],
-                          ["всё"]]
-        await update.message.reply_text(
-            "На каких дистанциях вы хотели бы участвовать?",
-            reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
-        ),
-        )
-        return DISTANCES
+		reply_keyboard = [["Горная", "Вело", "Пешеходная"],
+						  ["Охота на лис", "Водная"],
+						  ["всё"]]
+		await update.message.reply_text(
+			"На каких дистанциях вы хотели бы участвовать?",
+			reply_markup=ReplyKeyboardMarkup(
+			reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
+		),
+		)
+		return DISTANCES
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-    )
+	"""Cancels and ends the conversation."""
+	user = update.message.from_user
+	logger.info("User %s canceled the conversation.", user.first_name)
+	await update.message.reply_text(
+		"Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+	)
 
-    return ConversationHandler.END
+	return ConversationHandler.END
