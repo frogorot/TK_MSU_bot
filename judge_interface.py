245c4a6df34edefb4af4f1cde4c8535c63adfa68
evamoto <motovilina.anya@yandex.ru>
2022-10-08 21:27:41 +0300

    Update judge_interface.py
    Начала писать judge_interface

--- judge_interface.py
+++ judge_interface.py
@@ -39,10 +39,10 @@ logging.basicConfig(
 	filename="logs\py_log"+ datetime.today().strftime("-%m.%d.%Y,%H-%M-%S") + ".log",filemode="w"
 )
 logger = logging.getLogger(__name__)
-
+#################start_эту часть надо пересмотреть\переписать######
 #judge registration steps:
-AGE, GENDER, UNIVERSITY, FACILITY, DISTANCES = range(5)
-DISTANCES, STAGE, START, FINISH = range(5)
+#AGE, GENDER, UNIVERSITY, FACILITY, DISTANCES = range(5)
+DISTANCE, STAGE = range(2)
 
 # Handle '/start' and '/help'
 # Дописать!!
@@ -65,30 +65,62 @@ def processing_exceptions(message, Exception):
 
 # Handle '/ask_admin'
 #@bot.message_handler(commands=['ask_admin'])
-def process_ask_admin(message):
+
+async def process_ask_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text(
+	   'Пожалуйста, введите сообщение для Администратора.'
+	)
+	return MESSAGE_TO_ADMIN
+
+async def process_send_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text(
+	   'Ваше сообщение передано Администратору.'
+	)
+	if update.message.chat.has_protected_content or update.message.has_protected_content:
+		print("Unavalable")
+	# forward_message Не работает
 	try:
-		msg = bot.reply_to(message, 'Пожалуйста, введите сообщение для Администратора.')
-		bot.register_next_step_handler(msg, process_send_to_admin)
-	except Exception as e:
-		processing_exceptions(message, e)
 
-def process_send_to_admin(message):
-	try:
-		bot.forwardMessage(chat_id = admin_chat_id, from_chat_id = message.chat.id, message_id = message.message_id)
-		msg = bot.reply_to(message, 'Ваше сообщение передано Администратору.')
-	except Exception as e:
-		processing_exceptions(message, e)
-
-
+		await update.message.forward(chat_id= cf.admin_chat_id,
+					   #from_chat_id= update.message.chat.id,
+					   disable_notification = True
+					   #message_id= update.message.message_id
+					   )
+	except Exception as ex:
+		processing_exceptions(update.message, ex)
+	return ConversationHandler.END
+#################end_эту часть надо пересмотреть\переписать######
+admins = ['1728']
+async def notify(message):
+    command_sender = message.from_user.id
+    if command_sender in admins:
+        with open('judges_id.txt') as ids:
+            for line in ids:
+                user_id = int(line.strip("\n"))
+                try:
+                    bot.send_message(user_id,  f'уведомление от {command_sender}')
+                except Exception as e:
+                    bot.send_message(command_sender, f'ошибка отправки сообщения юзеру - {user_id}')
+    else:
+        bot.send_message(command_sender, f'у вас нет прав для запуска команды')
 async def judge_reg_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 	"""Starts the conversation and asks the user about their name."""
 	await update.message.reply_text(
 	   "Ура, рад вас видеть!"
 	   "Отправьте /cancel , чтобы прекратить общение.\n\n"
-	   "Как вас зовут? Пожалуйста, напишите полные имя и фамилию.",
+	   "Какую дистанцию вы судите?",
 	)
-	return AGE
+	return DISTANCE
 
+async def judge_reg_distance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+	"""Starts the conversation and asks the user about their name."""
+	await update.message.reply_text(
+	   "Какой этап вы судите",
+	)
+	reply_keyboard = [["Горная", "Вело", "Пешеходная"],
+					  ["Охота на лис", "Водная"],
+					  ["всё"]]
+	return STAGE
 async def user_reg_age(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 	"""Stores the selected name and asks for a age."""
 	user = update.message.from_user
@@ -100,33 +132,6 @@ async def user_reg_age(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
 
 	return GENDER
 
-async def user_reg_gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-	"""Stores the selected age and asks for a gender."""
-	user = update.message.from_user
-	logger.info("Age of %s: %s", user.first_name, update.message.text)
-
-	reply_keyboard = [["Мальчик", "Девочка"]]
-	
-	await update.message.reply_text(
-		"Пожалуйста, выберете ваш пол.",
-		reply_markup=ReplyKeyboardMarkup(
-			reply_keyboard, one_time_keyboard=True, input_field_placeholder="Мальчик или Девочка?"
-		),
-	)
-	
-	return UNIVERSITY
-
-async def user_reg_university(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-	"""Stores the selected gender and asks for a university."""
-	user = update.message.from_user
-	logger.info("Univer of %s: %s", user.first_name, update.message.text)
-	await update.message.reply_text(
-		"В каком вузе вы учитесь? "
-		"Если вы не студент, можете написать место работы или поставить \"-\".",
-		reply_markup=ReplyKeyboardRemove(),
-	)
-
-	return FACILITY
 
 # Надо переделать!!!
 async def user_reg_facility(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -165,7 +170,7 @@ async def user_reg_distances(update: Update, context: ContextTypes.DEFAULT_TYPE)
 
 		reply_keyboard = [["Горная", "Вело", "Пешеходная"],
 						  ["Охота на лис", "Водная"],
-						  ["всё"]]
+						  ["Всё"]]
 		await update.message.reply_text(
 			"На каких дистанциях вы хотели бы участвовать?",
 			reply_markup=ReplyKeyboardMarkup(
